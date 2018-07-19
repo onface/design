5b7cf052470b92174b46bb4760f3f93ed5b9eeab
Nimo Chu <nimo.jser@gmail.com>
2018-07-19 16:12:06 +0800

    dev

--- fis-conf.js
+++ fis-conf.js
@@ -2,10 +2,12 @@ fis.set('project.ignore', ['node_modules/**', 'output/**', 'fis-conf.js', 'docs/
 fis.match('/docs/**', {
     release: false
 })
-fis.hook(require('fis3-hook-relative'))
-fis.match('**', {
-    relative: true
-})
+if (fis.project.currentMedia() !== 'dev') {
+    fis.hook(require('fis3-hook-relative'))
+    fis.match('**', {
+        relative: true
+    })
+}
 fis.match('**.less', {
     parser: fis.plugin('less-2.x'),
     rExt:'css'
