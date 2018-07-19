fc0418d0b75d210bacae800820a9bb55aaf60d8c
Nimo Chu <nimo.jser@gmail.com>
2018-07-19 15:58:09 +0800

    relative

--- fis-conf.js
+++ fis-conf.js
@@ -2,6 +2,10 @@ fis.set('project.ignore', ['node_modules/**', 'output/**', 'fis-conf.js', 'docs/
 fis.match('/docs/**', {
     release: false
 })
+fis.hook(require('fis3-hook-relative'))
+fis.match('**', {
+    relative: true
+})
 fis.match('**.less', {
     parser: fis.plugin('less-2.x'),
     rExt:'css'
